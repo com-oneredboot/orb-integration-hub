fdfa7dfeaa1816f36ca74d6c5eba46675913f8d0
Corey Dale Peters <corey@thepetersfamily.ca>
2024-12-26 12:21:25 -0500

    fixing resolver

--- frontend/src/app/features/auth/store/auth.state.ts
+++ frontend/src/app/features/auth/store/auth.state.ts
@@ -3,15 +3,15 @@ import {User, UserGroup} from "../../../core/models/user.model";
 
 export enum AuthSteps {
   EMAIL,
-  PASSWORD,         // For existing users
-  PASSWORD_SETUP,   // For new users
-  EMAIL_VERIFY,
-  REGISTER,
-  PHONE_SETUP,
-  PHONE_VERIFY,
-  MFA_SETUP,
-  MFA_VERIFY,
-  COMPLETE
+  PASSWORD,           // For existing users
+  PASSWORD_SETUP,     // For new users
+  EMAIL_VERIFY,       // Verifies the email code
+  NAME_SETUP,         // for users without first_name, last_name
+  PHONE_SETUP,        // for users without phone_number
+  PHONE_VERIFY,       // verifies the phone number
+  MFA_SETUP,          // for users without MFA
+  MFA_VERIFY,         // verifies the MFA code
+  COMPLETE            // User setup is complete
 }
 
 export interface AuthState {
