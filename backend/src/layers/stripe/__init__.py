626404798d8082bd2ac67be532a08c39692acc54
Corey Dale Peters <corey@thepetersfamily.ca>
2025-08-08 16:42:37 -0400

    layers

--- backend/src/layers/stripe/__init__.py
+++
@@ -1,8 +0,0 @@
-# Stripe Layer
-"""
-Stripe SDK layer for payment processing.
-This layer only contains the Stripe Python SDK dependencies.
-"""
-
-# This layer is primarily for dependencies, no local modules to export
-__all__ = []
